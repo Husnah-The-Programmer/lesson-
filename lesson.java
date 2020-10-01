16ffd18e0e305c565d3fc1570a50dfdf96cf023d
Husnah-The-Programmer <67584836+Husnah-The-Programmer@users.noreply.github.com>
2020-10-01 13:06:03 +0100

    Add files via upload

---
+++ lesson.java
@@ -0,0 +1,18 @@
+
+import java.applet.Applet;
+import java.awt.Graphics;
+import java.awt.Font;
+import java.awt.color;
+
+
+class Lesson extends Applet
+{
+    public void paint(Graphics screen)
+    {
+        font f =new Font("Time Roman".fontITALIC,36);
+        screen.set Font(F);
+        color c =new color (40,80,120);
+        screen.set.color(c);
+        screen.draw string ("Husnah!!!!",100,60);
+    }
+}
